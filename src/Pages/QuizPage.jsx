5a2c43c72b714993eb15d9b69f15fdb3350f554f
hrjugar <hernanjugar123@gmail.com>
2022-09-28 22:23:54 +0800

    Added routes

---
+++ src/Pages/QuizPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function QuizPage() {
+    return (
+        <div className="quiz">
+            <h1>Quiz Page</h1>
+        </div>
+    );
+}
---
+++ src/Pages/ResultPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ResultPage() {
+    return (
+        <div className="result">
+            <h1>Result Page</h1>
+        </div>
+    );
+}
