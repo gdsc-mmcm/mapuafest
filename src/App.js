01c30191a1f265097b572d46f6673f9aaddf944e
hrjugar <hernanjugar123@gmail.com>
2022-09-28 22:38:37 +0800

    Added sass in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import HomePage from './Pages/HomePage';
 import QuizPage from './Pages/QuizPage';
 import ResultPage from './Pages/ResultPage';
@@ -8,26 +6,10 @@ import ResultPage from './Pages/ResultPage';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      <Route exact path='/' element={<HomePage />} />
       <Route path='quiz' element={<QuizPage />} />
       <Route path='result' element={<ResultPage />} />
     </Routes>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./styles/styles.css";
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
 
